1dbea95d35135ede905813304675df11995db46a
akash1kumar20 <akashkumar2101sm@gmail.com>
2023-07-06 13:33:51 +0530

    Loading the application

--- src/App.js
+++ src/App.js
@@ -5,23 +5,24 @@ import "./App.css";
 
 function App() {
   const [movie, setMovies] = useState([]);
-  function movieData() {
-    fetch("https://swapi.dev/api/films")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformedMovies = data.results.map((item) => {
-          return {
-            id: item.episode_id,
-            title: item.title,
-            openingText: item.opening_crawl,
-            releaseDate: item.release_date,
-          };
-        });
-        setMovies(transformedMovies);
-      });
+  const [loading, setLoading] = useState(false);
+  async function movieData() {
+    setLoading(true);
+    const response = await fetch("https://swapi.dev/api/films");
+    const data = await response.json();
+
+    const transformedMovies = data.results.map((item) => {
+      return {
+        id: item.episode_id,
+        title: item.title,
+        openingText: item.opening_crawl,
+        releaseDate: item.release_date,
+      };
+    });
+    setMovies(transformedMovies);
     //function available by browser for fetch api call
+    setLoading(false);
+    //once the setMoives function is completed, we don't want to excute the setLoading function
   }
   return (
     <React.Fragment>
@@ -29,7 +30,10 @@ function App() {
         <button onClick={movieData}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movie} />
+        {!loading && <MoviesList movies={movie} />}
+        {!loading && movie.length === 0 && <p>No Movies Found</p>}
+
+        {loading && <p>Bus bhai 1 min me aa rha hu...</p>}
       </section>
     </React.Fragment>
   );
