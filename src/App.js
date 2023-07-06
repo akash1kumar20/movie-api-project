029f1f89af7c01946fa9306785d3262af1bd54b8
akash1kumar20 <akashkumar2101sm@gmail.com>
2023-07-06 15:11:34 +0530

    Handling Error Situation

--- src/App.js
+++ src/App.js
@@ -6,35 +6,50 @@ import "./App.css";
 function App() {
   const [movie, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
   async function movieData() {
     setLoading(true);
-    const response = await fetch("https://swapi.dev/api/films");
-    const data = await response.json();
-
-    const transformedMovies = data.results.map((item) => {
-      return {
-        id: item.episode_id,
-        title: item.title,
-        openingText: item.opening_crawl,
-        releaseDate: item.release_date,
-      };
-    });
-    setMovies(transformedMovies);
-    //function available by browser for fetch api call
+    setError(null);
+    //to clear the previous error
+    try {
+      const response = await fetch("https://swapi.dev/api/films");
+      if (!response.ok) {
+        throw new Error("Something went wrong ....Retrying");
+      }
+      const data = await response.json();
+      const transformedMovies = data.results.map((item) => {
+        return {
+          id: item.episode_id,
+          title: item.title,
+          openingText: item.opening_crawl,
+          releaseDate: item.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+      //function available by browser for fetch api call
+    } catch (error) {
+      setError(error.message);
+    }
     setLoading(false);
     //once the setMoives function is completed, we don't want to excute the setLoading function
+  }
+
+  let content = <p>No Movies Found</p>;
+  if (movie.length > 0) {
+    content = <MoviesList movies={movie} />;
+  }
+  if (error) {
+    content = <p>{error}</p>;
+  }
+  if (loading) {
+    content = <p>Bus bhai 1 min me aa rha hu...</p>;
   }
   return (
     <React.Fragment>
       <section>
         <button onClick={movieData}>Fetch Movies</button>
       </section>
-      <section>
-        {!loading && <MoviesList movies={movie} />}
-        {!loading && movie.length === 0 && <p>No Movies Found</p>}
-
-        {loading && <p>Bus bhai 1 min me aa rha hu...</p>}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
